56677e268cbfe85925067d5ea4a3ef8c4a0753b2
zTwotz <nguyenngoctinh011258@gmail.com>
2026-04-05 16:10:00 +0700

    feat(graph-sync): PAC-574 add Neo4j health check

--- backend/src/neo4j/neo4j.module.ts
+++ backend/src/neo4j/neo4j.module.ts
@@ -1,9 +1,11 @@
 import { Module, Global } from '@nestjs/common';
 import { Neo4jService } from './neo4j.service';
+import { Neo4jController } from './neo4j.controller';
 
 @Global()
 @Module({
   providers: [Neo4jService],
   exports: [Neo4jService],
+  controllers: [Neo4jController],
 })
 export class Neo4jModule {}
